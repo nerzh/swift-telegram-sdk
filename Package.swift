5bcd7c2fa3a431d07676a4366944aaec8a6edd0b
Oleh Hudeichuk <emptystamp@gmail.com>
2021-06-08 11:16:19 +0300

    change name

--- Package.swift
+++ Package.swift
@@ -3,21 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "telegram-vapor-bot-lib",
+    name: "telegram-vapor-bot",
     platforms: [
         .macOS(.v10_15)
     ],
     products: [
         .library(
-            name: "telegram-vapor-bot-lib",
-            targets: ["telegram-vapor-bot-lib"]),
+            name: "telegram-vapor-bot",
+            targets: ["telegram-vapor-bot"]),
     ],
     dependencies: [
         .package(name: "vapor", url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.45.0")),
     ],
     targets: [
         .target(
-            name: "telegram-vapor-bot-lib",
+            name: "telegram-vapor-bot",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
             ]
