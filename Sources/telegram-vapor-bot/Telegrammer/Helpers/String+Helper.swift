f69a1b7b90a7fd546f78cbcffbaa215788dbf4a9
Higher08 <68398868+Higher08@users.noreply.github.com>
2022-01-30 20:16:17 +0200

    Count UTF16 instead of string

--- Sources/telegram-vapor-bot/Telegrammer/Helpers/String+Helper.swift
+++ Sources/telegram-vapor-bot/Telegrammer/Helpers/String+Helper.swift
@@ -76,7 +76,7 @@ public extension Int {
 public extension String {
     func matchRegexp(pattern: String) -> Bool {
         guard let regexp = try? NSRegularExpression(pattern: pattern, options: []) else { return false }
-        let range = NSRange(location: 0, length: self.count + 1)
+        let range = NSRange(location: 0, length: self.utf16.count)
         return regexp.numberOfMatches(in: self, options: [], range: range) != 0
     }
 }
