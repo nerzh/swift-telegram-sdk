4cc63dc142450ee67dce083b7a1ca3d67548986c
Giacomo Leopizzi <giacomo@leopizzi.com>
2023-01-13 01:29:23 +0200

    Scope fix

--- Sources/TelegramVaporBot/Bot/TGBot-Handlers.swift
+++ Sources/TelegramVaporBot/Bot/TGBot-Handlers.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension TGBot {
     
-    func onCallback(pattern: String, callback: @escaping (_ update: TGUpdate, _ bot: TGBotPrtcl) async throws -> TGAnswerCallbackQueryParams) {
+    public func onCallback(pattern: String, callback: @escaping (_ update: TGUpdate, _ bot: TGBotPrtcl) async throws -> TGAnswerCallbackQueryParams) {
         let handler = TGCallbackQueryHandler(pattern: pattern) { update, bot in
             let params = try await callback(update, bot)
             try bot.answerCallbackQuery(params: params)
@@ -17,12 +17,12 @@ extension TGBot {
         connection.dispatcher.add(handler)
     }
     
-    func onCommand(_ commands: String..., filters: TGFilter = .all, options: TGCommandHandler.Options = [], botUsername: String? = nil, callback: @escaping TGHandlerCallback) {
+    public func onCommand(_ commands: String..., filters: TGFilter = .all, options: TGCommandHandler.Options = [], botUsername: String? = nil, callback: @escaping TGHandlerCallback) {
         let handler = TGCommandHandler(commands: commands, filters: filters, options: options, botUsername: botUsername, callback)
         connection.dispatcher.add(handler)
     }
     
-    func onMessage(filters: TGFilter = .all, options: TGMessageHandler.Options = [.messageUpdates, .channelPostUpdates], callback: @escaping TGHandlerCallback) {
+    public func onMessage(filters: TGFilter = .all, options: TGMessageHandler.Options = [.messageUpdates, .channelPostUpdates], callback: @escaping TGHandlerCallback) {
         let handler = TGMessageHandler(filters: filters, options: options, callback)
         connection.dispatcher.add(handler)
     }
