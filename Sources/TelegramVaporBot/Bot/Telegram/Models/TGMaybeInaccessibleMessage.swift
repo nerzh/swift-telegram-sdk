cbb30de0c9db927f05cce9e06e1b14c5f15d99e8
Hadi Sharghi <hsharghi@gmail.com>
2024-01-09 14:39:32 +0330

    add common properties of TGMessage and TGInaccessibleMessage to TGMaybeInaccessibleMessage via extension

--- Sources/TelegramVaporBot/Bot/Telegram/Models/TGMaybeInaccessibleMessage.swift
+++ Sources/TelegramVaporBot/Bot/Telegram/Models/TGMaybeInaccessibleMessage.swift
@@ -12,3 +12,33 @@ public enum TGMaybeInaccessibleMessage: Codable {
     case message(TGMessage)
     case inaccessibleMessage(TGInaccessibleMessage)
 }
+
+
+public extension TGMaybeInaccessibleMessage {
+    var chat: TGChat {
+        switch self {
+        case .inaccessibleMessage(let msg):
+            return msg.chat
+        case .message(let msg):
+            return msg.chat
+        }
+    }
+    
+    var messageId: Int {
+        switch self {
+        case .inaccessibleMessage(let msg):
+            return msg.messageId
+        case .message(let msg):
+            return msg.messageId
+        }
+    }
+    
+    var date: Int {
+        switch self {
+        case .inaccessibleMessage(let msg):
+            return msg.date
+        case .message(let msg):
+            return msg.date
+        }
+    }
+}
