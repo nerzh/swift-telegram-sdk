aae73e4bd17eb0b7f6ad58bd59e4f95d1e63f504
Maxim-Lanskoy <lanskoyma@gmail.com>
2024-07-07 18:42:57 +0300

    Minor dependency fix.

--- Bot-Examples/FlyingFox-AsyncHttpClient/Package.swift
+++ Bot-Examples/FlyingFox-AsyncHttpClient/Package.swift
@@ -3,14 +3,12 @@
 import PackageDescription
 
 let packageDependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-alpha.1"),
     .package(url: "https://github.com/swhitty/FlyingFox.git", .upToNextMajor(from: "0.14.0")),
     .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.6.0"),
     .package(url: "https://github.com/nerzh/swift-telegram-sdk", .upToNextMajor(from: "3.0.3")),
 ]
 
 let targetDependencies: [PackageDescription.Target.Dependency] = [
-    .product(name: "Hummingbird", package: "hummingbird"),
     .product(name: "FlyingFox", package: "FlyingFox"),
     .product(name: "AsyncHTTPClient", package: "async-http-client"),
     .product(name: "SwiftTelegramSdk", package: "swift-telegram-sdk")
