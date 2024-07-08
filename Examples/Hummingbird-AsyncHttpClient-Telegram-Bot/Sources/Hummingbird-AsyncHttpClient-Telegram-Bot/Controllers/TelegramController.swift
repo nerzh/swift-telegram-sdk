6855b7dd823086961623117d63c025ecd15110f1
nerzh <emptystamp@gmail.com>
2024-07-08 07:55:11 +0200

    fix controller

--- Examples/Hummingbird-AsyncHttpClient-Telegram-Bot/Sources/Hummingbird-AsyncHttpClient-Telegram-Bot/Controllers/TelegramController.swift
+++ Examples/Hummingbird-AsyncHttpClient-Telegram-Bot/Sources/Hummingbird-AsyncHttpClient-Telegram-Bot/Controllers/TelegramController.swift
@@ -13,7 +13,9 @@ final class TelegramController {
     func addRoutes(to group: RouterGroup<some RequestContext>) {
         group.post("telegramWebHook") { req, context in
             let update: TGUpdate = try await req.decode(as: TGUpdate.self, context: context)
-            await botActor.bot.dispatcher.process([update])
+            Task {
+                await botActor.bot.dispatcher.process([update])
+            }
             return Response(status: 200)
         }
     }
